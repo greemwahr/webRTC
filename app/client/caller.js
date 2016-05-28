9472ede0621fb0086cf625875b5c498da2c191ee
shadowsyntax <tibetianwolf@gmail.com>
2016-05-29 00:16:56 +0100

    Scaffolding of the single webpage for the peer2peer application

--- app/client/caller.js
+++ app/client/caller.js
@@ -1,66 +1,68 @@
-function initCaller(messageCallback){
-    var RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription;
-    var RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
-    var RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate;
+function initCaller(messageCallback) {
+  var RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription;
+  var RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection
+  || window.mozRTCPeerConnection;
+  var RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate;
 
-    var wsUri = "ws://localhost:8090/";
-    var signalingChannel = createSignalingChannel(wsUri, CALLER_ID);
-    var servers = {iceServers: [{urls: "stun:stun.1.google.com:19302"}]};
-    
-    function startCommunication(peerId) {
-        var pc = new RTCPeerConnection(servers, {
-            optional: [{
-                DtlsSrtpKeyAgreement: true
-            }]
-        });
-        
-        signalingChannel.onAnswer = function (answer, source) {
-            console.log('receive answer from ', source);
-            pc.setRemoteDescription(new RTCSessionDescription(answer));
-        };
+  var wsUri = 'ws://localhost:8090/';
+  var signalingChannel = createSignalingChannel(wsUri, CALLER_ID);
+  var servers = { iceServers: [{ urls: 'stun:stun.1.google.com:19302' }] };
 
-        signalingChannel.onICECandidate = function (ICECandidate, source) {
-            console.log("receiving ICE candidate from ",source);
-            pc.addIceCandidate(new RTCIceCandidate(ICECandidate));
-        };
+  function startCommunication(peerId) {
+    var pc = new RTCPeerConnection(servers, {
+      optional: [{
+        DtlsSrtpKeyAgreement: true,
+      },],
+    });
 
-        pc.onicecandidate = function (evt) {
-            if(evt.candidate){ // empty candidate (wirth evt.candidate === null) are often generated
-                signalingChannel.sendICECandidate(evt.candidate, peerId);
-            }
-        };
+    signalingChannel.onAnswer = function (answer, source) {
+      console.log('receive answer from ', source);
+      pc.setRemoteDescription(new RTCSessionDescription(answer));
+    };
 
-        //:warning the dataChannel must be opened BEFORE creating the offer.
-        var _commChannel = pc.createDataChannel('communication', {
-            reliable: false
-        });
+    signalingChannel.onICECandidate = function (ICECandidate, source) {
+      console.log('receiving ICE candidate from ', source);
+      pc.addIceCandidate(new RTCIceCandidate(ICECandidate));
+    };
 
-        pc.createOffer(function(offer){
-            pc.setLocalDescription(offer);
-            console.log('send offer');
-            signalingChannel.sendOffer(offer, peerId);
-        }, function (e){
-            console.error(e);
-        });
+    pc.onicecandidate = function (evt) {
+      if (evt.candidate) { // empty candidate (wirth evt.candidate === null) are often generated
+        signalingChannel.sendICECandidate(evt.candidate, peerId);
+      }
+    };
 
-        window.channel = _commChannel;
-        
-        _commChannel.onclose = function(evt) {
-            console.log("dataChannel closed");
-        };
+    //:warning the dataChannel must be opened BEFORE creating the offer.
+    var _commChannel = pc.createDataChannel('communication', {
+      reliable: false,
+    });
 
-        _commChannel.onerror = function(evt) {
-            console.error("dataChannel error");
-        };
+    pc.createOffer(function (offer) {
+      pc.setLocalDescription(offer);
+      console.log('send offer');
+      signalingChannel.sendOffer(offer, peerId);
+    }, function (e) {
 
-        _commChannel.onopen = function(){
-            console.log("dataChannel opened");
-        };
+      console.error(e);
+    });
 
-        _commChannel.onmessage = function(message){
-            messageCallback(message.data);
-        };
-    }
-    
-    window.startCommunication = startCommunication;
+    window.channel = _commChannel;
+
+    _commChannel.onclose = function (evt) {
+      console.log('dataChannel closed');
+    };
+
+    _commChannel.onerror = function (evt) {
+      console.error('dataChannel error');
+    };
+
+    _commChannel.onopen = function () {
+      console.log('dataChannel opened');
+    };
+
+    _commChannel.onmessage = function (message) {
+      messageCallback(message.data);
+    };
+  }
+
+  window.startCommunication = startCommunication;
 }
